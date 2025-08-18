07e84d1b396e420700406b81d949d0b748f91e90
Hong <hong@dmp.es>
2025-08-18 13:35:41 +0200

    feat: add initial Terraform configuration for static website hosting (S3 + CloudFront + ACM + Route 53)

---
+++ 01-static-website-hosting/terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name prefix for resources"
+  type        = string
+  default     = "static-site"
+}
+
+variable "domain_name" {
+  description = "Custom domain name (example.com)"
+  type        = string
+}
+
+variable "hosted_zone_id" {
+  description = "Route 53 Hosted Zone ID for the domain"
+  type        = string
+}
