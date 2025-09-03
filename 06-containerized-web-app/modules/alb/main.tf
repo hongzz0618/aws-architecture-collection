8a4f123d81c6c2744fbe6950a0d84f3b0d046e98
Hong Zhou <hong.zhou@inetum.com>
2025-09-03 09:29:08 +0200

    fix(06): aws_lb_target_group target_type = "ip"

--- 06-containerized-web-app/modules/alb/main.tf
+++ 06-containerized-web-app/modules/alb/main.tf
@@ -29,6 +29,7 @@ resource "aws_lb_target_group" "this" {
   port     = var.target_group_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "ip"
 }
 
 resource "aws_lb_listener" "this" {
