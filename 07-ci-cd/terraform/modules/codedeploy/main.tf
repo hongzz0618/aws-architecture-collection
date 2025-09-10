ce74e62e99ca8b1354373baf6445946769a4f2a5
Hong Zhou <hong.zhou@inetum.com>
2025-09-10 16:03:11 +0200

    fix: aws_codedeploy_deployment_group sintax error

--- 07-ci-cd/terraform/modules/codedeploy/main.tf
+++ 07-ci-cd/terraform/modules/codedeploy/main.tf
@@ -4,13 +4,13 @@ resource "aws_codedeploy_app" "app" {
 }
 
 resource "aws_codedeploy_deployment_group" "dg" {
-  app_name = aws_codedeploy_app.app.name
+  app_name               = aws_codedeploy_app.app.name
   deployment_group_name = var.deployment_group
-  service_role_arn = var.service_role_arn
+  service_role_arn      = var.service_role_arn
 
   deployment_config_name = "CodeDeployDefault.AllAtOnce"
 
-  auto_scaling_groups = [ var.asg_name ]
+  autoscaling_groups = [var.asg_name]
 }
 
 output "application_name" { value = aws_codedeploy_app.app.name }
