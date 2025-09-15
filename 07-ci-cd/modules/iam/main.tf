0cb9ddbef84d23caf4c291b48e50e0240c084493
Hong <hong@dmp.es>
2025-09-15 23:57:32 +0200

    fix(07): cicd iam permissions

--- 07-ci-cd/modules/iam/main.tf
+++ 07-ci-cd/modules/iam/main.tf
@@ -1,6 +1,7 @@
-# CodeBuild Role
+# CodeBuild IAM Role
 resource "aws_iam_role" "codebuild" {
   name = "${var.project_name}-codebuild-role"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -11,14 +12,21 @@ resource "aws_iam_role" "codebuild" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "codebuild_attach" {
+# Attach managed policies for CodeBuild
+resource "aws_iam_role_policy_attachment" "codebuild_attach1" {
+  role       = aws_iam_role.codebuild.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_attach2" {
   role       = aws_iam_role.codebuild.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-# CodePipeline Role
+# CodePipeline IAM Role
 resource "aws_iam_role" "codepipeline" {
   name = "${var.project_name}-codepipeline-role"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -29,11 +37,18 @@ resource "aws_iam_role" "codepipeline" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "codepipeline_attach" {
+# Attach managed policies for CodePipeline
+resource "aws_iam_role_policy_attachment" "codepipeline_attach1" {
   role       = aws_iam_role.codepipeline.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineServiceRole"
 }
 
+resource "aws_iam_role_policy_attachment" "codepipeline_attach2" {
+  role       = aws_iam_role.codepipeline.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+# Outputs
 output "codebuild_role_arn" {
   value = aws_iam_role.codebuild.arn
 }
