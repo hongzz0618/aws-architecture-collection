9a3cbe720a1d2625871c14fe36292c6cb2971a80
Hong Zhou <hong.zhou@inetum.com>
2025-09-23 17:41:40 +0200

    fix(09): duplicated output model

--- 09-ml-deployment/modules/model-upload/main.tf
+++ 09-ml-deployment/modules/model-upload/main.tf
@@ -6,7 +6,3 @@ resource "aws_s3_object" "dummy_model" {
   # Create a valid tar.gz file content that SageMaker will accept
   content_base64 = "H4sIAAAAAAAAA+3PQQqDMBQF0L1P8Q+4cKFu3IkbFyKlTW1Dm4Q0Fdy9m2rFhYv3wTk8OAwDp2nipmmiYRjSvu9T13Wpbdv0bNs2NU2T6rpOVVWlsixTUfykKIp0z/M8ZVmWsiyLWZbFLMtilmUxy7KYZVnMsixmWRazLItZlsUsy2KWZTHLsphlWcyyLGZZFrMsi1mWxSzLYpZlMcuy+BcAAP//AwDe1WIlACAAAA=="
 }
-
-output "model_upload_complete" {
-  value = aws_s3_object.dummy_model.etag
-}
